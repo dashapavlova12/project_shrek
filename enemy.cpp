764285197526f964070feed0ed1212e774643d43
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2020-05-22 20:20:16 +0300

    Create enemy.cpp

---
+++ enemy.cpp
@@ -0,0 +1,114 @@
+#include "enemy.h"
+#include "map.h"
+#include <iostream>
+
+Enemy::Enemy(Image &image, float X, float Y,int W,int H,String Name):Entity(image,X,Y,W,H,Name)
+{
+    if (name == "EasyEnemy"){
+        sprite.setTextureRect(IntRect(0, 0, w, h));
+        dx = 0.025;
+        moveTimer = 0;
+        onGround = false;
+
+    }
+
+    roarBuffer.loadFromFile("music/Tiger6.wav");
+    roar.setBuffer(roarBuffer);
+
+}
+void Enemy::collision(Entity* enemy){
+    if (enemy -> name == "EasyEnemy" || enemy -> name == "HardEnemy")
+    {
+        if (dx > 0)
+            x -= w / 2;
+        else x += w / 2;
+    }
+    else if (enemy -> name == "Player1")
+    {
+        roar.play();
+        if (dx > 0)
+            x -= w/2;
+        else x +=w/2;
+
+        if (abs(y - enemy->y) < 100 && abs(x - enemy -> x) < 70 &&
+            !(enemy -> onGround)) {
+
+            health = 0;
+        }
+    }
+}
+void Enemy::checkCollisionWithMap(float Dx, float Dy)
+{
+    float next_position_x = x + dx;
+    int j = next_position_x / 32;
+    int i = (y)/32;
+
+    if (TileMap[i][j] == ' ') {
+        dy = 0.2;
+        y = i * 32;
+    }
+    else if (TileMap[i][j] == '1' || TileMap[i][j] == '2' ||
+        TileMap[i][j] == '3' || TileMap[i][j] == '4' || TileMap[i][j] == '5'
+        || TileMap[i][j] == '6' || TileMap[i][j] == '0')
+    {
+        if (next_position_x  >  x + 20)
+            x = j*32 - 64;
+        else x = j*32 + 32;
+        dx *= -1;
+    }
+
+    else if (TileMap[i][j] == 'w' || TileMap[i][j] == 'c')
+    {
+        speed = 0;
+        onGround = false;
+        health = 0;
+    }
+}
+
+void Enemy::control(float time)
+{
+    if (dx < 0) {
+        sprite.move(0, 0.1 * time);
+        play_animation(3, 0, 40, 42, 0, 1, time);
+        sprite.setScale(-1.0f, 1.0f);
+    }
+
+    if (dx > 0) {
+        sprite.move(0.1 * time, 0);
+        play_animation(3, 0, 40, 42, 0, 1, time);
+        sprite.setScale(1.0f, 1.0f);
+    }
+
+}
+
+void Enemy::update(float time)
+{
+    if (name == "EasyEnemy"){
+
+        control(time);
+        moveTimer += time;
+
+        if (moveTimer > 8000)
+        {
+            dx *= -1;
+            moveTimer = 0;
+        }
+
+        checkCollisionWithMap(dx, 0);
+        x += dx*time;
+
+        checkCollisionWithMap(0, dy);
+
+
+        sprite.setPosition(x + w/2 , y + h / 2);
+
+        if (health <= 0)
+        {
+            dx = 0;
+            dy = 0;
+            speed = 0;
+            life = false;
+        }
+    }
+
+}
