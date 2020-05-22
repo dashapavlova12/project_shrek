5c291420f5ed5036e6e5adef917611161f479629
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2020-05-22 22:40:34 +0300

    Create DependInfo.cmake

---
+++ cmake-build-debug/CMakeFiles/project.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dashapavlova/Рабочий стол/project/enemy.cpp" "/home/dashapavlova/Рабочий стол/project/cmake-build-debug/CMakeFiles/project.dir/enemy.cpp.o"
+  "/home/dashapavlova/Рабочий стол/project/entity.cpp" "/home/dashapavlova/Рабочий стол/project/cmake-build-debug/CMakeFiles/project.dir/entity.cpp.o"
+  "/home/dashapavlova/Рабочий стол/project/main.cpp" "/home/dashapavlova/Рабочий стол/project/cmake-build-debug/CMakeFiles/project.dir/main.cpp.o"
+  "/home/dashapavlova/Рабочий стол/project/map.cpp" "/home/dashapavlova/Рабочий стол/project/cmake-build-debug/CMakeFiles/project.dir/map.cpp.o"
+  "/home/dashapavlova/Рабочий стол/project/player.cpp" "/home/dashapavlova/Рабочий стол/project/cmake-build-debug/CMakeFiles/project.dir/player.cpp.o"
+  "/home/dashapavlova/Рабочий стол/project/view.cpp" "/home/dashapavlova/Рабочий стол/project/cmake-build-debug/CMakeFiles/project.dir/view.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
