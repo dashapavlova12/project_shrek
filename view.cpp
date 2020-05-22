dcad741a8d0118057cb6c868b68836656444199c
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2020-05-22 20:29:06 +0300

    Create view.cpp

---
+++ view.cpp
@@ -0,0 +1,19 @@
+//
+// Created by dashapavlova on 23.04.20.
+//
+#include "view.h"
+
+void setPlayerCoordinateForView(float x,float y) {
+
+
+    float tempX = x; float tempY = y;//считываем коорд игрока и проверяем их, чтобы убрать края
+
+    if (x < 320) tempX = 320;//убираем из вида левую сторону
+    if (y < 240) tempY = 240;//верхнюю сторону
+    if (y > 554) tempY = 554;//нижнюю сторону
+
+    view.setCenter(tempX, tempY); //следим за игроком, передавая его координаты.
+    //view.setViewport(sf::FloatRect(0,0.05,1,1));
+
+
+}
