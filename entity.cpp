707b20e8f7d303371ac660a6b8fd401404ea3b8c
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2020-05-22 20:21:29 +0300

    Create entity.cpp

---
+++ entity.cpp
@@ -0,0 +1,37 @@
+#include <SFML/Graphics.hpp>
+#include "entity.h"
+
+using namespace sf;
+bool Entity::shrek_near(Entity* shrek){}
+Entity::Entity(Image &image, float X, float Y,int W,int H,String Name){
+
+    currentFrame = 0;
+    x = X;
+    y = Y;
+    w = W;
+    h = H;
+    name = Name;
+    speed = 0; health = 100; dx = 0; dy = 0;
+    life = true; onGround = false; isMove = false;
+    texture.loadFromImage(image);
+    sprite.setTexture(texture);
+    sprite.setOrigin(w / 2, h / 2);
+}
+
+FloatRect Entity::getRect(){
+    return {x, y, (float)w, (float)h};
+}
+
+void Entity::play_animation(int num, int shift, int height, int width, int down, int cycle, float time){
+    currentFrame += 0.005 * time;
+    if (cycle)
+    {
+        if (currentFrame > num) currentFrame -= num;
+
+    }
+
+    sprite.setTextureRect(IntRect(width * int(currentFrame) + shift, down , width, height));
+    //sprite.move(0, 0.1 * time);
+
+
+}
