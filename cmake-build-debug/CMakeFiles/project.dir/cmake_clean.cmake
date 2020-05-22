2d6f57e6b735b8d73f2718d52af54ddc6582a3f5
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2020-05-22 22:43:48 +0300

    Create cmake_clean.cmake

---
+++ cmake-build-debug/CMakeFiles/project.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project.dir/main.cpp.o"
+  "CMakeFiles/project.dir/player.cpp.o"
+  "CMakeFiles/project.dir/entity.cpp.o"
+  "CMakeFiles/project.dir/map.cpp.o"
+  "CMakeFiles/project.dir/view.cpp.o"
+  "CMakeFiles/project.dir/enemy.cpp.o"
+  "project.pdb"
+  "project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
